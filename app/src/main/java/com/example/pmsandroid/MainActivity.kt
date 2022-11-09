e00fb56897adfc5db23eec5b119963e4970463e9
coderwdd <coderwdd@gmail.com>
2022-11-09 21:52:23 +0800

    Activity hideSystemStatusBar Function add app theme confirm

--- app/src/main/java/com/example/pmsandroid/MainActivity.kt
+++ app/src/main/java/com/example/pmsandroid/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.pmsandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.common.expand.hideSystemStatusBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        hideSystemStatusBar()
     }
 }
